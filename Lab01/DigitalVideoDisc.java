efdee54f750c51fdb9aaadfce0935d6628229519
hungtd036205000405 <hungtd036205@gmail.com>
2025-03-22 09:32:53 +0700

    Add for release

---
+++ Lab01/DigitalVideoDisc.java
@@ -0,0 +1,8 @@
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;// đạo diễn
+    private int length;// độ dài phim
+    private float cost;// giá tiền
+
+}
